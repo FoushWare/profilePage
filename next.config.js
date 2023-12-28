ac73a60555de31f4f34abef07abad94f2b80fc40
foushware <ahmed.fouad@reachnetwork.co>
2023-12-28 22:11:46 +0200

    adjust the envs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    REACT_APP_PRIVATE_KEY: process.env.NEXT_PUBLIC__APP_PRIVATE_KEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/modules/shared/configurations/axiosConfig.ts
+++ src/modules/shared/configurations/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // create axios instance
-const PrivateKey = process.env.REACT_APP_PRIVATE_KEY;
+const PrivateKey = process.env.NEXT_PUBLIC__APP_PRIVATE_KEY;
 const axiosInstance = axios.create({
   baseURL: "https://staging.mazaady.com/api/v1/",
   timeout: 1000,
